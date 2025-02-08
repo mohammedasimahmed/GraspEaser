42637b022250b4107fb6cc00866f374b93055e19
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-08 15:41:08 +0000

    chore: updated url_handler controller
    made the url_handler controller function as asynchronous,
    added request, response as parameters to the controller
    function and added URL check for youtube link.

--- apps/backend/src/controllers/url_handler.ts
+++ apps/backend/src/controllers/url_handler.ts
@@ -1,5 +1,13 @@
-function url_handler(){
+import { Request, Response } from "express"
 
+async function url_handler(request: Request, response: Response) {
+    const url: string = request.body()
+    if (url.includes("youtube")) {
+
+    }
+    else {
+
+    }
 }
 
 export default url_handler
