cafb42dfee99b95c0042143d370ac5b1bdb4f0eb
ecxtacy <dc.dhruvchouhan@gmail.com>
2025-02-09 11:56:10 +0530

    feat: http status code enum
    To provide better response syntax while sending
    response from handlers/middlewares.

---
+++ apps/backend/src/enums/http_status_code.ts
@@ -0,0 +1,29 @@
+export enum HttpStatusCode {
+  // Successful responses
+  OK = 200,
+  CREATED = 201,
+  ACCEPTED = 202,
+  NO_CONTENT = 204,
+
+  // Redirection messages
+  MOVED_PERMANENTLY = 301,
+  FOUND = 302,
+  NOT_MODIFIED = 304,
+
+  // Client error responses
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  METHOD_NOT_ALLOWED = 405,
+  REQUEST_TIMEOUT = 408,
+  CONFLICT = 409,
+  PAYLOAD_TOO_LARGE = 413,
+  UNSUPPORTED_MEDIA_TYPE = 415,
+
+  // Server error responses
+  INTERNAL_SERVER_ERROR = 500,
+  NOT_IMPLEMENTED = 501,
+  SERVICE_UNAVAILABLE = 503,
+  GATEWAY_TIMEOUT = 504,
+}
