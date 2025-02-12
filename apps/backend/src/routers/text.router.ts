3742dd73404b0cca22bc5151982c83569bc9e27e
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-12 07:56:46 +0000

    fix: changed the route of text handler

--- apps/backend/src/routers/text.router.ts
+++ apps/backend/src/routers/text.router.ts
@@ -3,6 +3,6 @@ import { text_handler } from "../controllers/text_handler.controller";
 
 const text_router = express.Router();
 
-text_router.post("/text", text_handler);
+text_router.post("/", text_handler);
 
 export default text_router;
