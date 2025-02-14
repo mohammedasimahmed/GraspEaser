cea6fd6331e2d575340c9679d98f94145080ad47
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-14 16:35:34 +0000

    fix: created /file route for file upload

--- apps/backend/src/routers/versions/v1.router.ts
+++ apps/backend/src/routers/versions/v1.router.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import url_router from "../url.router";
 import text_router from "../text.router";
+import file_router from "../file.router";
 
 const v1_router = express.Router();
 
 v1_router.use("/url", url_router);
 v1_router.use("/text", text_router);
+v1_router.use("/file", file_router);
 
 export default v1_router;
