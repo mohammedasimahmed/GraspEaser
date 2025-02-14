2f5f5dce25883eea87a0315c6003faba58cbdeb3
ecxtacy <dc.dhruvchouhan@gmail.com>
2025-02-14 16:21:46 +0530

    fix(env): prevent undefined API_KEY

--- apps/backend/src/config/env.ts
+++ apps/backend/src/config/env.ts
@@ -7,7 +7,7 @@ const env = {
     "postgresql://postgres:password@localhost:5432/mydb?schema=public",
   PORT: process.env.PORT || 5000,
   FRONTEND_URLS: [process.env.FRONTEND_URL || "http://localhost:3000"],
-  API_KEY: process.env.API_KEY,
+  API_KEY: process.env.API_KEY || "",
 };
 
 console.log(env);
