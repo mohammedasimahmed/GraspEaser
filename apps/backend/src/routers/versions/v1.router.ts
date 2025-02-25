00a2d14923cede1ac9c49f919a065c0029c16d2a
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-25 19:53:01 +0000

    feat: added user router

--- apps/backend/src/routers/versions/v1.router.ts
+++ apps/backend/src/routers/versions/v1.router.ts
@@ -2,11 +2,13 @@ import express from "express";
 import url_router from "../url.router";
 import text_router from "../text.router";
 import file_router from "../file.router";
+import user_router from "../user.router";
 
 const v1_router = express.Router();
 
 v1_router.use("/url", url_router);
 v1_router.use("/text", text_router);
 v1_router.use("/file", file_router);
+v1_router.use("/user", user_router);
 
 export default v1_router;
