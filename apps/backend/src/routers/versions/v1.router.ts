d90c1ae29a3ccbb10bf39b4dc0f4b235c262b85b
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-26 09:20:10 +0000

    fix: changed user router to auth router

--- apps/backend/src/routers/versions/v1.router.ts
+++ apps/backend/src/routers/versions/v1.router.ts
@@ -2,13 +2,13 @@ import express from "express";
 import url_router from "../url.router";
 import text_router from "../text.router";
 import file_router from "../file.router";
-import user_router from "../user.router";
+import auth_router from "../auth.router";
 
 const v1_router = express.Router();
 
 v1_router.use("/url", url_router);
 v1_router.use("/text", text_router);
 v1_router.use("/file", file_router);
-v1_router.use("/user", user_router);
+v1_router.use("/auth", auth_router);
 
 export default v1_router;
