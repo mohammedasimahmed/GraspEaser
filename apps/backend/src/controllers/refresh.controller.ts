871592d3f277424124fafe94da825912e0c31fb8
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-26 11:33:07 +0000

    feat(auth): generate new access token using refresh token
    - Implemented handler to generate a new access
      token when the old one expires using the
      refresh token.
    - Added validation for cookies and refresh token.
    - Included error handling for missing or invalid
      refresh token.

---
+++ apps/backend/src/controllers/refresh.controller.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken"
+import env from "../config/env";
+import { HttpStatusCode } from "../enums/http_status_code";
+import { ApiError } from "../lib/errors/api_error";
+
+export async function refresh_handler(req: Request, res: Response, next: NextFunction) {
+    if (!req.cookies) {
+        const cookieMissing = new ApiError(
+            "No cookies found",
+            HttpStatusCode.BAD_REQUEST
+        )
+        next(cookieMissing)
+        return;
+    }
+    const cookies = req.cookies;
+
+    if (!cookies?.jwt) {
+        const refreshTokenMissing = new ApiError(
+            "Refresh token missing in cookies",
+            HttpStatusCode.FORBIDDEN
+        )
+        next(refreshTokenMissing)
+        return;
+    }
+
+    const refreshToken = cookies.jwt;
+    try {
+        const user = jwt.verify(refreshToken, env.REFRESH_TOKEN_SECRET) as JwtPayload;
+
+        const accessToken = jwt.sign({
+            username: user.name,
+            email: user.email,
+            password: user.password
+        }, env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' })
+
+        res.status(HttpStatusCode.OK).json({ accessToken })
+    } catch (error) {
+        next(error)
+    }
+}
--- apps/backend/src/routers/auth.router.ts
+++ apps/backend/src/routers/auth.router.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { signup_handler } from "../controllers/signup.controller";
 import { signin_handler } from "../controllers/signin.controller";
+import { refresh_handler } from "../controllers/refresh.controller";
 
 const auth_router = express.Router();
 
 auth_router.post("/signup", signup_handler)
 auth_router.post("/signin", signin_handler)
+auth_router.get("/refresh", refresh_handler)
 
 export default auth_router;
