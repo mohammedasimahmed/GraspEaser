e966eac90615fabff3fd6724163e5945163f30ba
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-27 03:56:04 +0000

    feat: configure CORS to allow credentials

--- apps/backend/src/config/config.ts
+++ apps/backend/src/config/config.ts
@@ -3,6 +3,7 @@ import env from "./env";
 
 export const cors_config: CorsOptions = {
   origin: env.FRONTEND_URLS,
+  credentials: true, 
 };
 
 export const TEMPORARY_FILE_UPLOAD_DEST = ".uploads/";
