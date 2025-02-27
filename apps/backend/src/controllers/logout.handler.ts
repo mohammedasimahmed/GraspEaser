919a3e69479c08f5d310e68bfe3509ad926b2c46
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-27 03:19:12 +0000

    feat: add logout functionality in backend

---
+++ apps/backend/src/controllers/logout.handler.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpStatusCode } from "../enums/http_status_code";
+import { ApiError } from "../lib/errors/api_error";
+
+export async function logout_handler(req: Request, res: Response, next:NextFunction) {
+    if (!req.cookies) {
+        const cookieMissing = new ApiError(
+            "No cookies found",
+            HttpStatusCode.BAD_REQUEST
+        )
+        next(cookieMissing)
+        return;
+    }
+    const cookies = req.cookies;
+
+    if (!cookies?.refresh) {
+        const refreshTokenMissing = new ApiError(
+            "Refresh token missing in cookies",
+            HttpStatusCode.UNAUTHORIZED
+        )
+        next(refreshTokenMissing)
+        return;
+    }
+
+    res.clearCookie("refresh", {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+        maxAge: 0
+    })
+
+    res.status(HttpStatusCode.OK).json({ message: "Logged out successfully" })
+}
--- apps/backend/src/routers/auth.router.ts
+++ apps/backend/src/routers/auth.router.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { signup_handler } from "../controllers/signup.controller";
 import { signin_handler } from "../controllers/signin.controller";
 import { refresh_handler } from "../controllers/refresh.controller";
+import { logout_handler } from "../controllers/logout.handler";
 
 const auth_router = express.Router();
 
 auth_router.post("/signup", signup_handler)
 auth_router.post("/signin", signin_handler)
 auth_router.get("/refresh", refresh_handler)
+auth_router.get("/logout", logout_handler)
 
 export default auth_router;
