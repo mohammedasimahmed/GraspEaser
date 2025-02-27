296e9b67e1fcfb418a173706b1524bc6938e4e31
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-27 10:36:05 +0000

    chore: handle token verification errors with custom response

--- apps/backend/src/middlewares/verifyToken.ts
+++ apps/backend/src/middlewares/verifyToken.ts
@@ -6,7 +6,7 @@ import env from "../config/env";
 
 export async function verifyToken(req: Request, res: Response, next: NextFunction) {
     const token = req.headers.authorization?.split(" ")[1];
-    
+
     if (!token) {
         const tokenMissing = new ApiError(
             "No token provided",
@@ -20,6 +20,24 @@ export async function verifyToken(req: Request, res: Response, next: NextFunctio
         jwt.verify(token, env.ACCESS_TOKEN_SECRET);
         next();
     } catch (error) {
-        next(error);
+        if (error instanceof jwt.TokenExpiredError) {
+            res.status(HttpStatusCode.UNAUTHORIZED).json({
+                message: "Access token expired",
+                tokenExpired: true
+            });
+            return;
+        } else if (error instanceof jwt.JsonWebTokenError) {
+            res.status(HttpStatusCode.UNAUTHORIZED).json({
+                message: "Invalid access token",
+                invalidToken: true
+            });
+            return;
+        }
+
+        res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+            message: "An error occurred while verifying the access token",
+            tokenError: true
+        })
+        return;
     }
 }
