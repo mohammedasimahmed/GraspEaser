dabd564a61e15742f641770bf43cd0bec76a0d4a
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-28 11:04:55 +0000

    chore: use generate_output function in text_content_handler to generate AI response

--- apps/backend/src/lib/text_content_handler.ts
+++ apps/backend/src/lib/text_content_handler.ts
@@ -2,26 +2,24 @@ import { scrape_webpage } from "./scraper";
 import { google_llm_langchain } from "./gen_ai";
 import { UserInputDataOptions } from "@repo/common/request";
 import { generate_prompt } from "./prompts";
+import { generate_output } from "./generate_output";
+import { splitText } from "./splitText";
 
 export async function text_content_handler(
   content: string,
   options: UserInputDataOptions,
+  username: string
 ) {
-  const llm = google_llm_langchain;
-
-  const ai_response = await llm.invoke([
-    ["system", generate_prompt(options)],
-    ["human", content],
-  ]);
-
-  return ai_response.content.toString();
+  const chunksArray = await splitText(content);
+  return generate_output(chunksArray, username, options);
 }
 
 export async function scrape_page_and_handle_text(
   url: string,
   options: UserInputDataOptions,
+  username: string
 ) {
   const content: string = await scrape_webpage(url);
-  const response = await text_content_handler(content, options);
+  const response = await text_content_handler(content, options, username);
   return response;
 }
