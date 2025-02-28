1ee106af0a611f40f36ee4be1d00c5ecdf5d621f
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-28 11:03:53 +0000

    feat: implement function to generate AI response

---
+++ apps/backend/src/lib/generate_output.ts
@@ -0,0 +1,28 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { vectorSaveAndSearch } from "./vectorSaveAndSearch";
+import { generate_prompt } from "./prompts";
+import { UserInputDataOptions } from "@repo/common/request";
+
+export async function generate_output(chunksArray: any, username: string, options: UserInputDataOptions) {
+    const prompt = generate_prompt(options);
+    const searches = await vectorSaveAndSearch(chunksArray, prompt, username);
+
+    let content = "";
+    searches?.forEach((search) => {
+        content = content + "\n\n" + search.pageContent;
+    });
+
+    const llm = new ChatGoogleGenerativeAI({
+        model: "gemini-1.5-pro",
+        temperature: 0,
+        maxRetries: 2,
+        apiKey: process.env.API_KEY,
+    });
+
+    const ai_response = await llm.invoke([
+        ["system", prompt],
+        ["human", content],
+    ]);
+    
+    return ai_response.content.toString();
+}
