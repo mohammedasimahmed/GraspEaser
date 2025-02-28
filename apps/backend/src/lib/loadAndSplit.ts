61f65cc14cb7a98b3406be33826b1980eee5e5a1
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-28 10:59:29 +0000

    feat: add function to load and split pdf/docx files

---
+++ apps/backend/src/lib/loadAndSplit.ts
@@ -0,0 +1,17 @@
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { DocxLoader } from "@langchain/community/document_loaders/fs/docx";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+
+export const loadAndSplit = async (file_path: string, type: string) => {
+    const loader = type === 'pdf' ? new PDFLoader(file_path) : new DocxLoader(file_path);
+    const file = await loader.load();
+
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 500,
+        chunkOverlap: 0,
+    });
+
+    const chunksArray = await textSplitter.splitDocuments(file);
+    console.log(chunksArray.length)
+    return chunksArray;
+};
