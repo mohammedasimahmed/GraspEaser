3d323121e565e93f28250f5fed7ce96fb5737df7
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-28 11:03:02 +0000

    feat(vector-store): implement vector save and search for user-specific memory

---
+++ apps/backend/src/lib/vectorSaveAndSearch.ts
@@ -0,0 +1,19 @@
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { TaskType } from "@google/generative-ai";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { userMemoryStoreMap } from "./user_memory_store_map";
+
+export const vectorSaveAndSearch = async (chunksArray: any, prompt: string, username: string) => {
+    const embeddings = new GoogleGenerativeAIEmbeddings({
+        model: "text-embedding-004",
+        taskType: TaskType.RETRIEVAL_DOCUMENT,
+        title: "Document title",
+        apiKey: process.env.API_KEY
+    });
+
+    const vectorStore = await MemoryVectorStore.fromDocuments(chunksArray, embeddings);
+    userMemoryStoreMap.set(username, vectorStore);
+
+    const searches = await userMemoryStoreMap.get(username)?.similaritySearch(prompt);
+    return searches;
+};
