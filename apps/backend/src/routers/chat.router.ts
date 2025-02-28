5f8b801aaf8a70f95be77967b1b5804c1592c66c
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-02-28 17:31:45 +0000

    feat: add chat controller

--- apps/backend/src/routers/chat.router.ts
+++ apps/backend/src/routers/chat.router.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import { chat_handler } from "../controllers/chat.controller";
 
 const chat_router = express.Router();
+
+chat_router.post("/", chat_handler);
 
 export default chat_router;
